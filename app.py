e40458783c0148b9bd577ab68c0279b36ca205dc
stefan <steopa80@gmail.com>
2024-05-20 18:32:43 +0300

    added dummy appand Docker for wrapping it

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/green')
+def green():
+    return jsonify('GreenApi')
+
+@app.route('/red')
+def red():
+    return jsonify('RedApi')
+
+@app.route('/')
+def blank():
+    return jsonify('u have red or green api to use')
+if __name__ == '__main__':
+    app.run(debug=True, port=4949)
