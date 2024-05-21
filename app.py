deedf5c1bb02058abea1585b12d0ee57c0357d9d
A5p1x <111125164+A5p1x@users.noreply.github.com>
2024-05-21 10:51:46 +0300

    Update app.py for ga checks

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, make_response
 
 app = Flask(__name__)
 
@@ -12,6 +12,21 @@ def red():
 
 @app.route('/')
 def blank():
-    return jsonify('u have red or green api to use test')
+    html_content = '''
+    <html>
+        <body>
+            <p>You have the following APIs to use:</p>
+            <ul>
+                <li><a href="/red">Red API</a></li>
+                <li><a href="/green">Green API</a></li>
+            </ul>
+        </body>
+    </html>
+    '''
+    response = make_response(html_content)
+    response.headers['Content-Type'] = 'text/html'
+    return response
+
 if __name__ == '__main__':
     app.run(debug=True, port=4949)
+
